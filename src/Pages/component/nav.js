3fe405f8079395bebf4c37ed1b18cf3f62bdae1d
abdul061 <godofcoc346346@gmail.com~>
2024-11-05 16:40:41 +0530

    nav change

--- src/Pages/component/nav.js
+++ src/Pages/component/nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../css/course.css';
 import { NavLink } from "react-router-dom";
+import "../../../public/assests/js/nav.js"
 
 const Navbar = () => {
   return (
