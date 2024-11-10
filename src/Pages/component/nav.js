a77c06b4927e64829a3626853ba49f81b5a62720
abdul061 <godofcoc346346@gmail.com~>
2024-11-10 15:01:49 +0530

    nav change

--- src/Pages/component/nav.js
+++ src/Pages/component/nav.js
@@ -1,66 +1,42 @@
-import React, { useState } from "react";
-import "../css/course.css";
-import { NavLink } from "react-router-dom";
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import '../css/nav.css'
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(false)
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
 
   return (
-    <div
-      data-collapse="medium"
-      data-animation="default"
-      data-duration="400"
-      data-wid="d0f4f9a2-f940-5462-6e29-953966fc44c9"
-      data-easing="ease"
-      data-easing2="ease"
-      role="banner"
-      className={`spark-square-menu-with-links-2 w-nav ${menuOpen ? "open" : ""}`}
-    >
-      <div className="spark-square-menu-inner-2">
-        <NavLink to="/" className="spark-brand-logo-2 w-nav-brand">
-          <img
-            loading="lazy"
-            height="50"
-            alt=""
-            src={`${process.env.REACT_APP_PUBLIC}/assests/img/Logo.jpeg`}
-          />
-        </NavLink>
-        <nav role="navigation" className={`spark-nav-menu-2 w-nav-menu ${menuOpen ? "show" : ""}`}>
-          <NavLink to="/" className="spark-nav-link-2 w-nav-link" activeClassName="active" onClick={closeMenu}>
-            Home
-          </NavLink>
-          <NavLink to="/about" className="spark-nav-link-2 w-nav-link" activeClassName="active" onClick={closeMenu}>
-            About
-          </NavLink>
-          <NavLink to="/course" className="spark-nav-link-2 w-nav-link" activeClassName="active" onClick={closeMenu}>
-            Course
-          </NavLink>
-          <NavLink to="/contact" className="spark-nav-link-2 w-nav-link" activeClassName="active" onClick={closeMenu}>
-            Contact
-          </NavLink>
-          <NavLink to="/contact" className="spark-nav-link spark-button w-nav-link" activeClassName="active" onClick={closeMenu}>
-            Contact Us
-          </NavLink>
-        </nav>
-        <div
-          onClick={toggleMenu}
-          className="spark-simple-menu-button-2 w-nav-button"
-        >
-          <div className="spark-line-14 spark-simple-line"></div>
-          <div className="spark-line-13 spark-simple-line"></div>
-          <div className="spark-line-12 spark-simple-line"></div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={`${process.env.REACT_APP_PUBLIC_URL}/assets/img/website-logo.jpeg`} height={50}/>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
-  );
-};
+    </nav>
+  )
+}
 
-export default Navbar;
+export default Navbar
