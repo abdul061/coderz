c5d82992bae4f832001aaa3b2f935e6903cdb68a
abdul061 <godofcoc346346@gmail.com~>
2024-11-10 17:03:26 +0530

    nav change

--- src/Pages/component/Chat.js
+++ src/Pages/component/Chat.js
@@ -4,7 +4,7 @@ import '../css/ChatIcon.css'; // Link to your custom CSS file
 const ChatIcon = () => {
   return (
     <a
-      href="https://wa.me/+91 8072387798" // Replace with your WhatsApp number
+      href="https://wa.me/+918072387798" // Replace with your WhatsApp number
       target="_blank"
       rel="noopener noreferrer"
       className="chat-icon"
