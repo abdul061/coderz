a86503aacedf0f3afcd1cffb68466eedaf71c50d
abdul061 <godofcoc346346@gmail.com~>
2024-11-10 15:23:16 +0530

    nav change

--- src/Pages/component/nav.js
+++ src/Pages/component/nav.js
@@ -1,43 +1,50 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import Hamburger from 'hamburger-react'
-import '../css/nav.css'
+import { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import Hamburger from 'hamburger-react';
+import '../css/nav.css';
 
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
+  const location = useLocation();
 
+  // Function to toggle navbar visibility
   const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar)
-  }
+    setShowNavbar(!showNavbar);
+  };
+
+  // Close navbar when location (route) changes
+  useEffect(() => {
+    setShowNavbar(false);
+  }, [location]);
 
   return (
     <nav className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={`${process.env.REACT_APP_PUBLIC}/assests/img/Logo.jpeg`} height={50}/>
+          <img src={`${process.env.REACT_APP_PUBLIC}/assets/img/Logo.jpeg`} height={50} alt="Logo" />
         </div>
         <div className="menu-icon" onClick={handleShowNavbar}>
-          <Hamburger />
+          <Hamburger toggled={showNavbar} toggle={setShowNavbar} />
         </div>
-        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+        <div className={`nav-elements ${showNavbar ? 'active' : ''}`}>
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/" onClick={() => setShowNavbar(false)}>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink to="/about" onClick={() => setShowNavbar(false)}>About</NavLink>
             </li>
             <li>
-              <NavLink to="/course">Course</NavLink>
+              <NavLink to="/course" onClick={() => setShowNavbar(false)}>Course</NavLink>
             </li>
             <li>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink to="/contact" onClick={() => setShowNavbar(false)}>Contact</NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
