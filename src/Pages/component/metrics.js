375fa6d5fc8d100281960a3bc68a849a580e5e83
abdul061 <godofcoc346346@gmail.com~>
2024-11-11 14:20:14 +0530

    nav change

--- src/Pages/component/metrics.js
+++ src/Pages/component/metrics.js
@@ -3,23 +3,23 @@ import '../css/metrics.css'
 
 function metrics() {
   return (
-      <section className="features-metrics">
+      <section className="feat-metrics">
     <div className="container">
-      <div className="features-wrapper">
-        <div className="features-block">
-          <div className="features-title">500<span className="features-title-small">+</span></div>
+      <div className="feat-wrapper">
+        <div className="feat-block">
+          <div className="feat-title">500<span className="feat-title-small">+</span></div>
           <div>Enrolled Students </div>
         </div>
-        <div className="features-block">
-          <div className="features-title">90<span className="features-title-small">%</span></div>
+        <div className="feat-block">
+          <div className="feat-title">90<span className="feat-title-small">%</span></div>
           <div>success Rate </div>
         </div>
-        <div className="features-block">
-          <div className="features-title">20+</div>
+        <div className="feat-block">
+          <div className="feat-title">20+</div>
           <div>Course Offered</div>
         </div>
-        <div className="features-block">
-          <div className="features-title">2k<span className="features-title-small">+</span></div>
+        <div className="feat-block">
+          <div className="feat-title">2k<span className="feat-title-small">+</span></div>
           <div>Followers</div>
         </div>
       </div>
